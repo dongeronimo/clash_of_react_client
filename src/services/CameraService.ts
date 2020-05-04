da0f2817caa69862e9d7d2baa8f9e6dcd427b1e9
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-03 22:17:14 -0300

    The problem is that the distance between the eye and the look at point is changing when it shouldnt

--- src/services/CameraService.ts
+++ src/services/CameraService.ts
@@ -1,14 +1,6 @@
 import {PerspectiveCamera, Quaternion, Vector3} from "three";
 
 export default class CameraService {
-    // readonly cameraWorldPosition = new Vector3();
-    // readonly cameraWorldDirection = new Vector3();
-    // readonly cameraLookAt = new Vector3();
-    // readonly vUp = new Vector3();
-    // readonly vRight = new Vector3();
-    // readonly cameraQuaternion = new Quaternion();
-    // readonly camera: PerspectiveCamera;
-
     readonly camera: PerspectiveCamera;
     readonly cameraLookAt = new Vector3();
 
@@ -23,33 +15,30 @@ export default class CameraService {
     constructor(camera: PerspectiveCamera, lookAt: Vector3) {
         this.camera = camera;
         this.cameraLookAt.copy(lookAt);
-//        this.updateInnerState();
     }
 
     setLookAt = (lookAtPos:Vector3)=>{
         this.cameraLookAt.copy(lookAtPos);
         this.camera.lookAt(lookAtPos);
-        this.camera.updateMatrix();
-//        this.updateInnerState();
+
+        const cameraWorldDirection = new Vector3();
+        this.camera.getWorldDirection(cameraWorldDirection);
+        this.printVector("Camera World Direction", cameraWorldDirection);
     }
 
-    // private updateInnerState = () => {
-    //     this.camera.getWorldPosition(this.cameraWorldPosition);
-    //     this.camera.getWorldDirection(this.cameraWorldDirection);
-    //     this.cameraQuaternion.copy(this.camera.quaternion);
-    //     this.cameraLookAt.copy(this.cameraLookAt);
-    //     this.setVRight();
-    //     this.setVUp();
-    // }
 
     readonly rotateAroundLookUpPoint = (angleInDegs: number, axis: Vector3) => {
+        this.printVector("Look At", this.cameraLookAt)
         //1)Preciso pegar o vetor entre o foco e o olho, indo do foco pro olho.
         const cameraWorldDirection = new Vector3();
         const cameraWorldPosition = new Vector3();
         this.camera.getWorldDirection(cameraWorldDirection);
+        this.printVector("Camera World Direction", cameraWorldDirection);
         this.camera.getWorldPosition(cameraWorldPosition);
+        this.printVector("Camera World Position", cameraWorldPosition);
         const invertedNormalizedCameraDirectionVector = cameraWorldDirection.multiplyScalar(-1);
         const distanceBetweenLookAndCamera = cameraWorldPosition.sub(this.cameraLookAt).length();
+        console.log("distance ", distanceBetweenLookAndCamera);
         const invertedCameraDirection = invertedNormalizedCameraDirectionVector
             .multiplyScalar(distanceBetweenLookAndCamera);
         //2)Rotaciono o vetor da posicao da camera
@@ -60,17 +49,6 @@ export default class CameraService {
         //4)Refaz o lookat com o novo vetor
         this.camera.lookAt(this.cameraLookAt)
         this.camera.updateMatrix();
-        //this.updateInnerState();
-    }
-
-    private setVUp() {
-        this.vUp.set(0, 1, 0);
-        this.vUp.applyQuaternion(this.cameraQuaternion);
-    }
-
-    private setVRight() {
-        this.vRight.set(1, 0, 0);
-        this.vRight.applyQuaternion(this.cameraQuaternion);
     }
 
     printVector(text: string, vec: Vector3) {
