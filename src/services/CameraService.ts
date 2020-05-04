12f552406d27c857128da020b364593e16ca2847
luciano <luciano.geronimo.fnord@gmail.com>
2020-05-03 22:05:29 -0300

    Eliminated most states inside CameraService (that shouldnt be called a service because it is stateful, should be something like CameraHelper, CameraAuxilliary)

--- src/services/CameraService.ts
+++ src/services/CameraService.ts
@@ -1,52 +1,55 @@
 import {PerspectiveCamera, Quaternion, Vector3} from "three";
 
 export default class CameraService {
-    readonly cameraWorldPosition = new Vector3();
-    readonly cameraWorldDirection = new Vector3();
-    readonly cameraLookAt = new Vector3();
-    readonly vUp = new Vector3();
-    readonly vRight = new Vector3();
-    readonly cameraQuaternion = new Quaternion();
+    // readonly cameraWorldPosition = new Vector3();
+    // readonly cameraWorldDirection = new Vector3();
+    // readonly cameraLookAt = new Vector3();
+    // readonly vUp = new Vector3();
+    // readonly vRight = new Vector3();
+    // readonly cameraQuaternion = new Quaternion();
+    // readonly camera: PerspectiveCamera;
+
     readonly camera: PerspectiveCamera;
+    readonly cameraLookAt = new Vector3();
 
     static createCamera(eyeX:number, eyeY:number, eyeZ:number, lookX:number, lookY:number, lookZ:number,
                         width:number, height:number):PerspectiveCamera{
         const camera = new PerspectiveCamera(70, width / height, 0.01, 1000);
         camera.position.set(eyeX, eyeY, eyeZ);
         camera.lookAt(lookX, lookY, lookZ);
-
         return camera;
-    }
-
-    printVector(text: string, vec: Vector3) {
-        console.log(`${text}: ${vec.x}, ${vec.y}, ${vec.z}`);
     }
 
     constructor(camera: PerspectiveCamera, lookAt: Vector3) {
         this.camera = camera;
-        this.updateInnerState();
+        this.cameraLookAt.copy(lookAt);
+//        this.updateInnerState();
     }
 
     setLookAt = (lookAtPos:Vector3)=>{
         this.cameraLookAt.copy(lookAtPos);
         this.camera.lookAt(lookAtPos);
         this.camera.updateMatrix();
-        this.updateInnerState();
+//        this.updateInnerState();
     }
 
-    private updateInnerState = () => {
-        this.camera.getWorldPosition(this.cameraWorldPosition);
-        this.camera.getWorldDirection(this.cameraWorldDirection);
-        this.cameraQuaternion.copy(this.camera.quaternion);
-        this.cameraLookAt.copy(this.cameraLookAt);
-        this.setVRight();
-        this.setVUp();
-    }
+    // private updateInnerState = () => {
+    //     this.camera.getWorldPosition(this.cameraWorldPosition);
+    //     this.camera.getWorldDirection(this.cameraWorldDirection);
+    //     this.cameraQuaternion.copy(this.camera.quaternion);
+    //     this.cameraLookAt.copy(this.cameraLookAt);
+    //     this.setVRight();
+    //     this.setVUp();
+    // }
 
     readonly rotateAroundLookUpPoint = (angleInDegs: number, axis: Vector3) => {
         //1)Preciso pegar o vetor entre o foco e o olho, indo do foco pro olho.
-        const invertedNormalizedCameraDirectionVector = this.cameraWorldDirection.multiplyScalar(-1);
-        const distanceBetweenLookAndCamera = this.cameraWorldPosition.sub(this.cameraLookAt).length();
+        const cameraWorldDirection = new Vector3();
+        const cameraWorldPosition = new Vector3();
+        this.camera.getWorldDirection(cameraWorldDirection);
+        this.camera.getWorldPosition(cameraWorldPosition);
+        const invertedNormalizedCameraDirectionVector = cameraWorldDirection.multiplyScalar(-1);
+        const distanceBetweenLookAndCamera = cameraWorldPosition.sub(this.cameraLookAt).length();
         const invertedCameraDirection = invertedNormalizedCameraDirectionVector
             .multiplyScalar(distanceBetweenLookAndCamera);
         //2)Rotaciono o vetor da posicao da camera
@@ -57,7 +60,7 @@ export default class CameraService {
         //4)Refaz o lookat com o novo vetor
         this.camera.lookAt(this.cameraLookAt)
         this.camera.updateMatrix();
-        this.updateInnerState();
+        //this.updateInnerState();
     }
 
     private setVUp() {
@@ -70,5 +73,7 @@ export default class CameraService {
         this.vRight.applyQuaternion(this.cameraQuaternion);
     }
 
-
+    printVector(text: string, vec: Vector3) {
+        console.log(`${text}: ${vec.x}, ${vec.y}, ${vec.z}`);
+    }
 }
